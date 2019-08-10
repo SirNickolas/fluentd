937507c4b382f0cc563cad63f448b29e0d64ddfd
Nickolay Bukreyev <buknik95@yandex.ru>
2019-08-10 21:19:21 +0700

    Fix `scripts/update_syntax_fixtures.sh`

--- scripts/update_syntax_fixtures.sh
+++ scripts/update_syntax_fixtures.sh
@@ -5,13 +5,15 @@ IFS=$'\t\n'
 [ $# = 0 ] || exit 2
 cd -- "$(dirname -- "$0")"/..
 
-if cd fluent/; then
-    echo 'Pulling from projectfluent/fluent...'
-    git pull
-    cd - >/dev/null
-else
+if [ -n "$(find fluent/ -maxdepth 0 -empty)" ]; then
     echo 'Cloning from projectfluent/fluent...'
     git submodule update --init fluent
+else
+    echo 'Pulling from projectfluent/fluent...'
+    cd fluent
+    git checkout -q master
+    git pull
+    cd - >/dev/null
 fi
 
 echo 'Copying fixtures...'
