b8fbc5360e3a82144befd41fe2c5420f77168c5f
Nickolay Bukreyev <buknik95@yandex.ru>
2019-08-13 08:09:15 +0700

    Parse reference syntax test files and convert them to JSON

--- syntax/test/src/main.d
+++ syntax/test/src/main.d
@@ -1,13 +1,50 @@
-import std.stdio;
+import std.json;
 import stdf = std.file;
 
-import ftl = fluentd.syntax.parser;
+import ftl = fluentd.syntax;
 
-void main(string[ ] args) {
-    auto result = ftl.parse(stdf.readText(args[1]));
-    writefln(
-        "ParserResult(Resource([\n%(    %s,\n%|%)]), [\n%(    %s,\n%|%)])",
-        result.resource.body,
-        result.errors,
-    );
+private @safe:
+
+JSONValue parseJSONFile(string filename) @system {
+    import std.mmfile;
+    import std.typecons: scoped;
+
+    auto mmf = scoped!MmFile(filename);
+    return parseJSON(cast(const(char)[ ])(cast(MmFile)mmf)[ ]);
+}
+
+ftl.Resource parse(string source) nothrow pure {
+    return ftl.parse(source).resource;
+}
+
+void test(string filename) @system
+in {
+    import std.algorithm.searching: endsWith;
+
+    assert(filename.endsWith(".ftl"));
+}
+do {
+    import std.stdio: File;
+
+    // `stdf.readText` UTF-validates the file, and we don't want it to happen here.
+    immutable rc = parse(cast(string)stdf.read(filename));
+    immutable ourJSON = ftl.convertTo!JSONValue(rc);
+    File(filename[0 .. $ - 3] ~ "out.json", "w").writeln(ourJSON.toPrettyString());
+
+    const refJSON = parseJSONFile(filename[0 .. $ - 3] ~ "json");
+    // TODO: Compare `ourJSON` against `refJSON`.
+}
+
+int main() @system {
+    import std.stdio;
+
+    int ret = 0;
+    foreach (string filename; stdf.dirEntries("fixtures", "*.ftl", stdf.SpanMode.breadth))
+        try
+            test(filename);
+        catch (Exception e) {
+            writeln(e);
+            ret = 1;
+        }
+    return ret;
 }
